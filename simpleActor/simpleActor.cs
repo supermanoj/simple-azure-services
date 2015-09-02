491bbeebb80b7d8a937463f2db9c5cd45edfbbc3
unknown <manojsi@MANOJSI01.redmond.corp.microsoft.com>
2015-09-01 17:20:25 -0700

    adding simple actor stateless service

---
+++ simpleActor/simpleActor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using simpleActor.Interfaces;
+using Microsoft.ServiceFabric;
+using Microsoft.ServiceFabric.Actors;
+
+namespace simpleActor
+{
+    public class simpleActor : Actor, IsimpleActor
+    {
+        public async Task<string> DoWorkAsync()
+        {
+            ActorEventSource.Current.ActorMessage(this, "Doing Work");
+
+            return await Task.FromResult("Work result");
+        }
+    }
+}
---
+++ simple_azure_service.Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using simpleActor.Interfaces;
+using Microsoft.ServiceFabric.Actors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_azure_service.Client
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var proxy = ActorProxy.Create<IsimpleActor>(ActorId.NewId(), "fabric:/simple_azure_service");
+
+            Console.WriteLine("From Actor {0}: {1}", proxy.GetActorId(), proxy.DoWorkAsync().Result);
+        }
+    }
+}
